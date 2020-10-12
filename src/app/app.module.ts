fc38681f820df88cda50480393b4d95a69c6bb25
Akshita vijay <akshitavijay.cse20@jecrc.ac.in>
2020-10-12 19:19:28 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { EmployerComponent } from './employer/employer.component';
+import { JobseekerComponent } from './jobseeker/jobseeker.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    AdminComponent,
+    LoginComponent,
+    EmployerComponent,
+    JobseekerComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
